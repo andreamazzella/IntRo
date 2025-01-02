8c70f624d254c70b111f38b20349f5c87abd45c8
Andrea Mazzella <andrea.mazzella@ukhsa.gov.uk>
2025-01-02 14:36:30 +0000

    finished drafting 05_structures, but need to rename to 05_classes and add exercises.

--- 05_structures.R
+++ 05_structures.R
@@ -1,8 +1,20 @@
-
 # DRAFT
-# title: "04: Summarising data"
+# TODO: rename to 05_classes
+# title: "05: data classes"
 # subtitle: "Introduction to R for health data"
 # author: Andrea Mazzella [(GitHub)](https://github.com/andreamazzella)
+
+# Content
+# - Data classes
+#   - Logical
+#   - Numeric
+#   - Character
+#   - Factor
+#   - Date
+# - Comparing values
+# - Vectors
+# - Lists
+# - Data frames
 
 # Recap from topic 04_summarise  ---------------------------------------------------------------------------------------
 
@@ -11,33 +23,113 @@
 
 # Data classes ---------------------------------------------------------------------------------------------------------
 
-# TODO: can move stuff here from intro6 clean.
+# R represents different variable types (continuous, categorical etc.) internally using "classes".
 
-# Factors --------------------------------------------------------------------------------------------------------------
+# You can check what class something is by using class():
+class(0.78)
+class(NA)
+class("Maria")
 
-# TODO: can move stuff here from intro6 clean.
+# R data classes that are relevant to health data analysis are:
+# - logical,
+# - numeric (including integer and double),
+# - Date,
+# - character,
+# - factor
+# - lists
+# - data.frame (and tibble)
 
+## Logical ----------------------------------------------------------------------------------
+
+# Three possible values - note they're upper-case and NOT in quotes.
+TRUE
+T
+FALSE
+F
+NA
+
+# Note that FALSE is equivalent to 0 and TRUE is equivalent to 1:
+FALSE == 0
+TRUE == 1
+
+# They are created when running comparisons with > < >= <= == (NB: two = symbols) !=
+# as well as functions like is.na() and dplyr::near().
+4 > 7
+4 == 5 - 1
+"Maria" == NA
+is.na("Maria")
+near(1/49*49, 1)
+
+## Numeric ----------------------------------------------------------------------------------
+
+# Numeric includes integers and "doubles" (that is, real numbers).
+typeof(2.7)
+
+# Adding an L after an integer forces R to treat it as an integer
+typeof(2)
+typeof(2L)
+
+# NB: R can't store very large integers in the integer class 
+typeof(10000000000L)
+
+# You can use arithmetic operators and numeric functions
+7^3
+round(4.7)
+
+# Note that R uses "round half to even", which keeps the rounding unboased.
+round(0.5)
+round(1.5)
+
+# If you want the less precise rounding used in Stata, you can use janitor::round_half_up()
+janitor::round_half_up(0.5)
+
+## Character --------------------------------------------------------------------------------
+
+# stringr
+
+# Character strings are created using either double quotes (`"`) or single quotes (`'`).
+"Maria"
+'Maria'
+
+# The tidyverse style guide recommends using double quotes.
+
+# If you need to have quotes within quotes, you can use the other one.
+"A string with an internal 'quote' can be created like so."
+
+# You can use \n to create a new line.
+text <- "one\ntwo"
+str_view(text)
+
+# You can combine more strings with `paste0()` or `stringr::str_c()`
+paste0("Maria ", "Ramírez ", "Rioja")
+str_c("Maria ", "Ramírez ", "Rioja")
+
+# `stringr::str_c()` has the advantage of returning NA if one or more element is NA:
+paste0("Josep ", NA, "Dalí")
+str_c("Josep ", NA, "Dalí")
+
+# You can subset a character string by using `gsub()` or stringr::str_sub()`:
+str_sub("ID3487", 3, 6)
 
 # Vectors --------------------------------------------------------------------------------------------------------------
 
-# In R, a vector is a list of objects of the same type, in a specific order.
+# An R vector is a list of objects of the same type, in a specific order.
 
 # For example:
 # - TRUE TRUE FALSE is a logical vector
-# - 5 2 4 is a vector (of numbers)
-# - "cirrhosis" "fibrosis" "hepatitis" is a vector (of characters).
-
-# Of note, every column in a `data.frame` is a vector!
+# - 5 2 4 is a numeric vector
+# - "cirrhosis" "fibrosis" "hepatitis" is a character vector
     
 # You create a vector using function `c()`, which stands for "combine":
 c(TRUE, TRUE, FALSE)
 c(5, 2, 4)
 c("cirrhosis", "fibrosis", "hepatitis")
 
-# For regular sequences of numeric values, you can use `seq()`. This can be useful when setting regular x-axis breaks in time series charts.
+# To create a vector with a regular sequence of numeric values, you can use `seq()`.
+# This can be useful when setting regular x-axis breaks in time series charts.
 seq(from = 2010, to = 2020, by = 2)
 
-# If you want to increase by 1, you can use the colon as shorthand:
+# If you want to increase by 1, you can use the colon symbol as shorthand:
 2010:2015
 
 # Vectors can be used:
@@ -53,11 +145,72 @@ log(100, 5)
 
 # Objects in a vector can have names:
 c(
-  "age" = 19,
-  "height" = 185,
-  "systolic_BP" = 132
+  age = 19,
+  height = 185,
+  systolic_BP = 132
   )
 
+# Of note, every column in a `data.frame` is a vector - see section "Data frames" below for more information.
+
+# More data classes ----------------------------------------------------------------------------------------------------
+
+## Factors ----------------------------------------------------------------------------------
+
+# Factors are used to represent categorical variables: a variable in which each value can only be one of a series.
+
+# `forcats` is a package part of the core `tidyverse` and it helps dealing with factors.
+library(forcats)
+
+# A vector of character strings doesn't sort in a useful way:
+sample_months <- c("March", "January", "August")
+sort(sample_months)
+
+# We can create a factor with a specific order with `forcats::fct()` or `factor()`:
+sample_months_factor <-
+  fct(
+    sample_months,
+    c("January", "February", "March", "April", "May", "June", "July",
+      "August", "September", "October", "November", "December")
+    )
+
+# If we sort the vector, it will now sort correctly.
+sort(sample_months_factor)
+
+# You can use modify the order of levels in a factor with `forcats::fct_relevel()` to reorder bars or legend items in a ggplot.
+
+## Dates ------------------------------------------------------------------------------------
+
+# Dates are more complicated that one initially thinks as they are affected by leap years and other variables.
+
+# Package `lubridate` (part of the core `tidyverse`) helps working with dates.
+library(lubridate)
+# For example, you can create today's date with lubridate::today():
+today()
+
+# You can also convert a string into a date using `lubridate`'s helper functions:
+ymd("2025-01-02")
+dmy("01/04/2018")
+
+# You can extract components using `lubridate::year()`, `lubridate::month()`, `lubridate::day()` and other:
+year(today())
+month(ymd("2025-01-02"))
+day(dmy("01/04/2018"))
+
+# Time spans are implemented as durations, periods and intervals.
+
+## Year-months ------------------------------------------------------------------------------
+
+# If you commonly need to use monthly values, you can use the `zoo::yearmon` class.
+library(zoo)
+sample_date <- ymd("2025-05-01")
+sample_yearmon <- as.yearmon(sample_date)
+sample_yearmon
+as.numeric(sample_yearmon)
+
+## Year-quarters ----------------------------------------------------------------------------
+sample_yearqtr <- as.yearqtr(sample_date)
+sample_yearqtr
+as.numeric(sample_yearqtr)
 
 # Lists ----------------------------------------------------------------------------------------------------------------
 
@@ -84,22 +237,40 @@ list_study_info$nhs_trusts
 # Data frames ----------------------------------------------------------------------------------------------------------
 
 # You've already used `data.frame`s.
-# They are implemented as a named list of vectors of the same length: each column has a name and contains a vector of values.
+# They are implemented as named lists of vectors of the same length: each column has a name and contains a vector of values.
 dat <- data.frame(
-    id = 1001:1004,
-    initials = c("NG", "PT", "DG", "OG"),
-    age = c(36, 24, 4, 1),
-    sex = c("M", "F", "F", "M")
-    )
+  id = 1001:1004,
+  initials = c("NG", "PT", "DG", "OG"),
+  age = c(36, 24, 4, 1),
+  sex = c("M", "F", "F", "M")
+  )
 
-# As data.frames are lists, you can access a column by using `$`:
-dat$id
+# You can isolate a column as a vector in three ways:
+# - with `dplyr::pull()` - tidyverse friendly.
+# - with the exposition operator (`$`)
+# - with the `[[` operator
+dat |> pull(id)
 dat$initials
+dat[["age"]]
 
-# This is why you can use 
-mean(dat$age)
-table(dat$sex)
+# You can use these for quick summaries:
+summary(dat$age)
+dat |>
+  pull(age) |>
+  summary()
 
 # `tibble`s are a special type of `data.frame` used in the tidyverse.
 
+# You can create a tibble in an easy to read layout using `tibble::tribble()`:
+tribble(~id, ~initials,
+        1001, "NG",
+        1002, "PT")
 
+# Learn more ------------------------------------------------------------------------------------------------------------
+
+# https://r4ds.hadley.nz/logicals
+# https://r4ds.hadley.nz/numbers
+# https://r4ds.hadley.nz/strings
+# https://r4ds.hadley.nz/factors
+# https://r4ds.hadley.nz/datetimes
+# https://r4ds.hadley.nz/base-r
